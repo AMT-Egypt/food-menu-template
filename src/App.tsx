7869d403f8216a24eaba9b330cbeb6d09fc3fed1
ahmedmansour779 <ahmedmansour1239@gmail.com>
2024-08-12 18:31:14 +0300

    fix swiper and languge default

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import AllFoods from './components/AllFoods';
 import ButtonToTop from './components/ButtonToTop';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
+import { setLanguage } from './store/translation';
 import './style/App.css';
 import './style/scrollBar.css';
 function App() {
+  const dispatch = useDispatch()
+  const userLanguage = navigator.language;
+  useEffect(() => {
+    userLanguage == "ar" ? dispatch(setLanguage("ar")) : dispatch(setLanguage("en"))
+  }, [dispatch, userLanguage])
   return (
     <>
       <Header />
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,32 +1,33 @@
 import { IconSearch } from "@tabler/icons-react";
 import { useDispatch, useSelector } from "react-redux";
+import "swiper/css";
+import { Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { typesFoodDataAr } from "../../data/typesFoodDataAr";
 import { typesFoodDataEn } from "../../data/typesFoodEn";
 import { setSearchValue } from "../../store/search";
 import { RootState } from "../../types/translationTypes";
-import { Slider } from "../AMT_Package";
+
 export default function SearchBar() {
-  const { restaurantName, searchHere } = useSelector(
-    (state: RootState) => state.translation.words
-  );
+  const { restaurantName, searchHere } = useSelector((state: RootState) => state.translation.words);
   const lang = useSelector((state: RootState) => state.translation.language);
   const dispatch = useDispatch();
   const data = lang == "ar" ? typesFoodDataAr : typesFoodDataEn;
 
   return (
     <div
-      style={{ direction: lang == "ar" ? "rtl" : "ltr" }}
       className="flex flex-col justify-center items-center gap-6 w-full py-6 container mx-auto "
     >
       <div
-        className={`${
-          lang == "en" ? "font-primary" : "font-secondary"
-        } text-primary`}
+        style={{ direction: lang == "ar" ? "rtl" : "ltr" }}
+        className={`${lang == "en" ? "font-primary" : "font-secondary"} text-primary`}
       >
         <p>{restaurantName}</p>
       </div>
-      <div className="flex flex-col gap-4 w-full overflow-hidden">
-        <div className="w-full flex gap-1 border border-solid border-gray-300 text-secondary p-2 rounded-md">
+      <div className="flex flex-col gap-4 w-full">
+        <div
+          style={{ direction: lang == "ar" ? "rtl" : "ltr" }}
+          className="w-full flex gap-1 border border-solid border-gray-300 text-secondary p-2 rounded-md">
           <IconSearch />
           <input
             className="w-full bg-transparent outline-none"
@@ -35,34 +36,30 @@ export default function SearchBar() {
             placeholder={searchHere}
           />
         </div>
-        <div className="w-full overflow-x-scroll scroll-bar">
-          <div
-            style={{ width: "100%", textAlign: "center" }}
-            className="flex gap-2 overflow-scroll w-fit"
-          >
-            <Slider
-              autoplayInterval={2000}
-              autoplay
-              direction={lang === "ar" ? "rtl" : "ltr"}
-            >
-              {data.map(({ image, link, name }) => (
-                <a
-                  href={`#${name}`}
-                  key={link}
-                  className="w-32 h-40 overflow-hidden flex flex-col gap-1 text-primary"
-                >
-                  <div
-                    style={{
-                      backgroundImage: `url(${image})`,
-                    }}
-                    className="w-32 h-32 bg-cover bg-center border-primary border-solid border-2 rounded-lg"
-                  ></div>
-                  <p>{name}</p>
-                </a>
-              ))}
-            </Slider>
-          </div>
-        </div>
+        <Swiper
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+          centeredSlides={true}
+          slidesPerView={2}
+          spaceBetween={20}
+          loop
+          className="mySwiper w-full">
+          {data.map(({ image, link, name }, index) => (
+            <SwiperSlide key={index} className="text-center !flex justify-center">
+              <a href={`#${name}`} key={link} className="w-32 h-40 overflow-hidden flex flex-col gap-1 text-primary ">
+                <div style={{
+                  backgroundImage: `url(${image})`
+                }} className="w-32 h-32 bg-cover bg-center border-primary border-solid border-2 rounded-lg"></div>
+                <p>
+                  {name}
+                </p>
+              </a>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
